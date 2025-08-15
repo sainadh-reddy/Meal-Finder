52054fd1ee7df8fb87e012afc9a0296b9709074d
sainadh-reddy <sainadh1308@gmail.com>
2025-08-15 10:35:07 +0530

    Added Category Tumbnail images

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const categoryList = document.getElementById("categories");
 const hamburgerBtn = document.getElementById("hamburgerBtn");
+const categoryThumbList = document.getElementById("categoriesList");
 
 hamburgerBtn.addEventListener("click", () => {
   categoryList.classList.toggle("hidden");
@@ -29,16 +30,30 @@ fetch("https://www.themealdb.com/api/json/v1/1/categories.php")
     // Add each category
     categories.forEach(category => {
         const listItem = document.createElement("li");
-        const link = document.createElement("a");
-        link.textContent = category.strCategory;
-        link.href = "#"; // Placeholder, can be replaced with a click handler
-        link.className = "block text-gray-700 hover:text-orange-500";
-        listItem.className = "text-s px-4 hover:bg-gray-100 cursor-pointer";
-        listItem.appendChild(link);
+        listItem.className = "block text-gray-700 text-s px-4 hover:bg-gray-100 cursor-pointer hover:text-orange-500";
+        listItem.id = `category-${category.idCategory}`;
+        listItem.textContent = category.strCategory;
         const divider = document.createElement("hr");
         divider.className = "border-gray-300 my-1";
         categoryList.appendChild(listItem);
         categoryList.appendChild(divider);
+
+        // Create category thumbnail
+        const categoryThumb = document.createElement("div");
+        categoryThumb.className = `bg-white shadow-lg  relative text-center cursor-pointer`;
+        categoryThumb.id = `category-${category.idCategory}Thumb`;
+        const thumbImage = document.createElement("img");
+        thumbImage.src = category.strCategoryThumb;
+        thumbImage.alt = category.strCategory;
+        thumbImage.className = "w-full h-32 object-fit p-2";
+
+        const thumbTitle = document.createElement("div");
+        thumbTitle.textContent = category.strCategory;
+        thumbTitle.className = `absolute py-1 px-2 text-sm text-white bg-orange-500 rounded top-0 right-0 m-1`;
+
+        categoryThumb.appendChild(thumbImage);
+        categoryThumb.appendChild(thumbTitle);
+        categoryThumbList.appendChild(categoryThumb);
     });
   })
   .catch(error => {
